0f8311311946701b32f50c15ac324f96e7f29a72
Evgeny Golubev <limp061289@gmail.com>
2020-09-15 02:14:58 +0300

    modify burger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,21 @@
 //hamburger-menu
 
-const hamburgerButton = document.querySelector(".hamburger");
-const hamburgerMenu = document.querySelector(".menu");
-const menuCloser = document.querySelector(".menu-closer");
-const hero = document.querySelector(".hero");
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger");
+const links = document.querySelectorAll(".menu__link");
 
-
-hamburgerButton.addEventListener("click", e => {
-    e.preventDefault();
-    const hamburgerMenuClone = hamburgerMenu.cloneNode(true);
-    hero.appendChild(hamburgerMenuClone);
-    hamburgerMenuClone.style.display = "flex";
-
-    menuCloser.addEventListener("click", e => {
-        e.preventDefault();
-        hero.removeChild(hamburgerMenuClone);
-    });
+links.forEach(function(link) {
+    link.addEventListener('click', toggleMenu)
 });
+
+function toggleMenu() {
+    menu.classList.toggle("menu--overlay");
+    hamburger.classList.toggle("hamburger--close");
+}
+
+hamburger.addEventListener('click' , toggleMenu);
+
+
 
 //product parameters-button, dropdown menu
 
